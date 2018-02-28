665d63b60fbef4229659a4a395388ecb1e7abc04
SamJamCul <samueljamesculley@gmail.com>
2018-02-28 15:14:07 +0000

    finished basic calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(a, b)
+  return a + b
+end
+
+def subtract(a, b)
+  return a - b
+end
+
+def sum(a)
+  total = 0
+  a.length.times do |i|
+    total += a[i]
+  end
+  return total
+end
