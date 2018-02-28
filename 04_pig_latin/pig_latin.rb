2fdc1e46c1dbf10abdcb982f30bcf7037f7675be
SamJamCul <samueljamesculley@gmail.com>
2018-02-28 16:18:15 +0000

    finished timer

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,7 @@
 #write your code here
+def translate(string)
+  vowels = %w| a e i o u |
+  if vowels.include?(string[0]) == true
+    return string + "ay"
+  end
+end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    @hours = (seconds / 3600).to_s
+    @minutes = ((seconds % 3600) / 60).to_s
+    @seconds = ((seconds % 3600) % 60).to_s
+    def add0(var)
+      if var.length == 1
+        var.insert(0, "0")
+      end
+    end
+    add0(@hours)
+    add0(@minutes)
+    add0(@seconds)
+    @time_string = "#{@hours}:#{@minutes}:#{@seconds}"
+  end
 end
