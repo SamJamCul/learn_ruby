afcc0e4a83dd1268a234b47ab181f7c6b5427f28
SamJamCul <samueljamesculley@gmail.com>
2018-02-28 15:44:20 +0000

    finished simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(string)
+  return "#{string}"
+end
+
+def shout(string)
+  return "#{string.upcase}"
+end
+
+def repeat(string, n=2)
+  result = ("#{string} ") * n
+  return result[0..-2]
+end
+
+def start_of_word(string, n)
+  return string[0..n-1]
+end
+
+def first_word(string)
+  words = string.split(" ")
+  return words[0]
+end
+
+def titleize(string)
+  words = string.split
+  exceptions = ["and", "over", "the"]
+  words.length.times do |i|
+    if exceptions.include? words[i]
+    else
+      words[i] = words[i].capitalize
+    end
+  end
+  result = words.join(" ")
+  result[0] = result[0].upcase
+  return result
+end
